be2fae0534627aea533f33eefce6a3a325d17461
Ridma <ridmakaveendra7@gmail.com>
2022-12-13 07:06:04 +0530

    aa

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 import com.exceptions.EmptyArrayException;
 
 import java.security.spec.ECField;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
+        /*
         Calculator cal = new Calculator();
         List<Integer> list = List.of();
         try {
@@ -13,7 +16,21 @@ public class Main {
         }catch (EmptyArrayException e){
             System.out.println(e.getMessage());
         }
+         */
 
+
+
+
+        String x = "A String in Java is actually an object, which contain methods that can perform certain operations on strings. For example, the length of a string can be found with the length() method:";
+        String []y= x.split(" ");
+
+        String a = "";
+        
+        for (int i=0;i< y.length;i++){
+            a=a.concat(y[y.length-i-1]+" ");
+        }
+        System.out.println(a);
+        Collections.reverse(Arrays.asList(x.split(" ")))
 
     }
 }
